55e7497e714fbe2ee0f1f70cc628fda104cf2d78
nelsonSulter <nelsonsulter91@gmail.com>
2021-05-11 11:05:00 +0200

    Activation tornade WIP
    Il reste à ajouter les dégats infligés au joueur sur chaque goutte de la tornade

---
+++ Orion/Assets/Scripts/ECS/Systems/ActivateTornadoSystem.cs
@@ -0,0 +1,61 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using UnityEngine;
+
+
+public class ActivateTornadoSystem : ComponentSystem
+{
+    private float spawnTimer = 0f;
+    private Unity.Mathematics.Random random;
+    private float3 position;
+
+    protected override void OnCreate()
+    {
+        random = new Unity.Mathematics.Random(56);
+    }
+    protected override void OnUpdate()
+    {
+        
+        if (spawnTimer >= 5f)
+        {
+
+            position = new float3(random.NextFloat(-50f, 50f), 0f, random.NextFloat(-50f, 50f));
+
+            spawnTimer = 0;
+            
+            bool drap = true;
+            for (int i = 0; i < 500; i = i + 1)
+            {
+                
+                //On accède à notre entity grace à la variable globale du PrefabEntityComponent
+                Entity spawnedEntity = EntityManager.Instantiate(PrefabEntityComponent.waterDropPrefabEntity);
+
+                EntityManager.SetComponentData(spawnedEntity, new Translation { Value = new float3(position.x, position.y +( 0.10f * i), position.z) });
+
+
+                
+                if (drap)
+                {
+                    EntityManager.SetComponentData(spawnedEntity, new TornadoMovementData { duration = 15, initialPos = position, dropRotation = 1, timeCounter = 0f, speed = random.NextFloat(1,5), width = random.NextFloat(0.6f, 1f) * (1f + (0.05f * i)) , height = 7f });; ;
+
+                }
+                else
+                {
+                    EntityManager.SetComponentData(spawnedEntity, new TornadoMovementData { duration = 15, initialPos = position, dropRotation = -1, timeCounter = 0f, speed = random.NextFloat(1, 5), width = random.NextFloat(0.6f, 1f) * (1f + (0.05f * i)), height = 7f }); ;
+
+                }
+
+                drap = !drap;
+
+            }
+
+
+
+        }
+
+        spawnTimer = spawnTimer + Time.DeltaTime;
+
+    }
+
+}
---
+++ Orion/Assets/Scripts/ECS/Systems/TornadoMovementSystem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Burst;
+
+[AlwaysSynchronizeSystem]
+
+[UpdateAfter(typeof(ActivateTornadoSystem))]
+public class TornadoMovementSystem : JobComponentSystem
+{
+
+    // Start is called before the first frame update
+    [BurstCompile]
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+       
+        float deltatime = Time.DeltaTime;
+
+        // Ces variable vont servire à faire bouger nos sphère en cercles
+
+
+        // requête pour accéder à toutes les entities qui ont un component translation, rotation et MovementData
+        // La différence entre le mot clef ref et le mot clef in concerne la manière dont on accède à ces component
+        // Ref on a droit à la lecture et à l'écriture
+        // In veut dire qu'on ne peut que le lire.
+        // On fait bouger les sphères de haut en bas
+
+        EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+
+        Entities.ForEach((Entity e, ref Translation translation, ref Rotation rotation, ref TornadoMovementData tornadoMovementData) => {
+
+            tornadoMovementData.timeCounter = tornadoMovementData.timeCounter + deltatime * tornadoMovementData.speed;
+
+            translation.Value.x = tornadoMovementData.initialPos.x + (-1 * (Mathf.Cos(tornadoMovementData.timeCounter) *  tornadoMovementData.width));
+            translation.Value.z = tornadoMovementData.initialPos.z + (tornadoMovementData.dropRotation * (Mathf.Sin(tornadoMovementData.timeCounter) * tornadoMovementData.height)); // ajouter -1 devant ou non pour changer sens de rotation
+
+
+            tornadoMovementData.duration = tornadoMovementData.duration - deltatime;
+
+
+            if (tornadoMovementData.duration <= 0)
+            {
+                commandBuffer.AddComponent<ExplosionTag>(e);
+            }
+
+
+        }).Run();
+
+        commandBuffer.Playback(EntityManager);
+        commandBuffer.Dispose();
+
+        return default;
+    }
+}
